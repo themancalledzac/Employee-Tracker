478afc1d5a6811cd369a6bc6401d819e5aa30f0e
Zac Edens <themancalledzac@gmail.com>
2021-01-01 17:21:27 -0800

    updating initial menu to be a start inquirer that breaks into sub menus of view, create, update, and delete.

--- index.js
+++ index.js
@@ -6,7 +6,24 @@ const db = require("./db");
 // this removes the connection info from our main file, and-
 // hides it in our db folder away from prying eyes
 // const connection = require("./db/connection");
+const connection = require("./db/connection");
 
+// break up the start into an intial menu that further breaks down, so as not to have too many options from the start.
+function start() {
+
+    inquirer
+        .prompt({
+            message: "Choose your menu",
+            name: "Initial_Menu",
+            type: "list",
+            choices: [
+                "VIEW",
+                "CREATE",
+                "UPDATE",
+                "DELETE",
+            ]
+        })
+}
 
 
 function askForAction() {
@@ -20,7 +37,12 @@ function askForAction() {
                 "VIEW_DEPARTMENTS",
                 "VIEW_ROLES",
                 "VIEW_EMPLOYEES",
+                "CREATE_DEPARTMENT",
                 "CREATE_ROLE",
+                "CREATE_EMPLOYEE",
+                "UPDATE_EMPLOYEE",
+                "EMPLOYEES_BY_MANAGER",
+                "DELETE_DEPARTMENT",
                 "QUIT"
 
             ]
@@ -34,21 +56,43 @@ function askForAction() {
                     return;
 
                 case "VIEW_ROLES":
+                    viewRoles();
                     return;
 
                 case "VIEW_EMPLOYEES":
+                    viewEmployees();
                     return;
 
-                // case "CREATE_ROLE":
-                //     createRole();
-                //     return;
+                case "CREATE_DEPARTMENT":
+                    createDepartment();
+                    return;
+
+                case "CREATE_ROLE":
+                    createRole();
+                    return;
+
+                case "CREATE_EMPLOYEE":
+                    createEmployee();
+                    return;
+
+                case "UPDATE_EMPLOYEE":
+                    updateEmployee();
+                    return;
+
+                case "EMPLOYEES_BY_MANAGER":
+                    employeesByManager();
+                    return;
+
+                case "DELETE_DEPARTMENT":
+                    deleteDepartment();
+                    return;
 
                 default:
                     connection.end();
             }
 
         })
-}
+};
 
 function viewDepartments() {
 
@@ -59,31 +103,78 @@ function viewDepartments() {
             askForAction();
         });
 }
+function viewRoles() {
 
-// function createRole() {
-//     db
-//         .getDepartments()
-//         .then((departments) => {
+    db
+        .getRoles()
+        .then((results) => {
+            console.table(results);
+            askForAction();
+        });
+}
+function viewEmployees() {
 
-//             console.log(departments);
+    db
+        .getEmployees()
+        .then((results) => {
+            console.table(results);
+            askForAction();
+        });
+}
 
-//             console.log(
-//                 departments
-//             )
+// add department
+function createDepartment() {
 
-//             // inquirer
-//             //     .prompt([
-//             //         {
-//             //             message: "What department is this role for?",
-//             //             type: "list",
-//             //             choices: departments.map(department) => ({
-//             //                 value: department.id,
-//             //                 label: department.name
-//             //             })
-//             //         }
-//             //     ])
+};
 
-//         })
-// }
+// add role.
+//  
+function createRole() {
+    db
+        .getDepartments()
+        .then((departments) => {
+            console.log(departments);
+
+            inquirer
+                .prompt([
+                    {
+                        message: "What department is this role for?",
+                        type: "list",
+                        choices: 
+                    }
+                ])
+
+        })
+};
+
+// add employee
+function createEmployee() {
+
+};
+
+
+// update employee
+// also asks to update employee manager
+function updateEmployee() {
+
+};
+
+// view employees by manager
+function employeesByManager() {
+
+};
+
+// delete an entire department
+// probably will need a secondary confirmation, "are you SURE?"
+// do we need to force an update of department of people who's department is deleted?
+// 
+function deleteDepartment() {
+
+};
+
+
+// remove employee
+
+// update
 
 askForAction();
